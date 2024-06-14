e97b277b6cb006eee0ec60dbce82835027878ff6
steam <155938693+fcrownarisk@users.noreply.github.com>
2024-06-15 06:20:04 +0800

    Update zzz.js

--- xyz/zzz.js
+++ xyz/zzz.js
@@ -1,6 +1,6 @@
 void function zzz(){
     namespcacezzz={
-        z: z ^ 2 + z ^ 3 / z ^ (-1) + z * (-2) + z^(-3)
+        z: z ^ 2 + z ^ 3 / z ^ (-1) + z ^ (-2) + z ^ (-3)
     }
     return [z.z.z]
 }
