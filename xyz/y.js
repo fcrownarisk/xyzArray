7bd3a3412cd2ae18e5e0ade895fe8e4d74e5d025
steam <155938693+fcrownarisk@users.noreply.github.com>
2024-06-15 06:19:23 +0800

    Update y.js

--- xyz/y.js
+++ xyz/y.js
@@ -1,8 +1,7 @@
 declare.module.type
 declare.module.y
 void function y(){
-    constructor(y1=DOMStringList,y2=DOMPoint,
-        y3=DOMPointReadOnly,y4=DOMStringMap);{
+    constructor(y1=DOMStringList,y2=DOMPoint,y3=DOMPointReadOnly,y4=DOMStringMap);{
     typevoid(y1*y2)/(y3*y4)
     }
     return [y1,y2,y3,y4]
