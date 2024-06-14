f340917499d267cbd170878822101a69067dc792
steam <155938693+fcrownarisk@users.noreply.github.com>
2024-06-15 06:20:33 +0800

    Update zzzz.js

--- xyz/zzzz.js
+++ xyz/zzzz.js
@@ -1,7 +1,7 @@
 declare.module.zzzz
 void function zzzz(){
     namespacezzzz={
-        z:z^3+z^4+z^5/z^(-1)+z^(-2)
+        z:z ^ 3 + z ^ 4 + z ^ 5 / z ^ (-1) + z ^ (-2)
     }
     return [zzzz]
 }
